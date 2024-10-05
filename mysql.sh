e8b00a3375053fe09c191795203e575a35ba93e2
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-05 10:58:47 +0530

    mysql-file

--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,33 @@
 USER=$( id -u )
 TIME_STAMP=$( date +%F-%D-%M-%S )
 SCRIPT=$( echo $0 | cut -d "." -f1 )
-LOG=$SCRIPT-$TIME_STAMP
+LOG=/tmp/$SCRIPT-$TIME_STAMP
 
-echo " current file name is" $LOG
+if [ $USER -eq 0 ]
+then 
+    echo "you are the super user "
+else
+    echo "need super user access"
+    exit 1 # manually exing 
+fi 
+VALIDATE()
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 done successflly"
+    else
+        echo "$2 failed .. "
+        exit 1 # manually exiting 
+    fi 
+
+dnf install mysql-server -y &>> LOG
+VALIDATE $? "installing mysql server"
+
+systemctl start mysqld &>> LOG
+VALIDATE $? "starting mysql "
+
+systemctl enable mysqld &>> LOG
+VALIDATE $? "enabling mysql "
+
+mysql_secure_innstallation  --set -root-password ExpenseApp@1 
+VALIDATE  $? "settinng the root password"
+
