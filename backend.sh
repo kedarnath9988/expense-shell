b192276422da3b45f7d0e824dc5918b3f705d7ff
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-07 18:30:11 +0530

    frontend

--- backend.sh
+++ backend.sh
@@ -77,7 +77,7 @@ VALIDATE $? "start backend"
 dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "installing mysql clint"
 
-mysql -h db.dawskedarnath.omline -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+mysql -h db.dawskedarnath.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
 VALIDATE $? "loading the schema" 
 
 systemctl restart backend &>>$LOG_FILE
